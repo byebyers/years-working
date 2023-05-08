06ea221b5d61caf4d2c76f2df4d2a3e942d31573
byebyers <byers.jacob@gmail.com>
2023-05-08 13:51:47 -0600

    gap-y instead of justify-between

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const [totalYears, setTotalYears] = useState(0)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-y-5 p-24">
       <h1>I have been working for {totalYears} years</h1>
       <Years technology={data.technology} yearResult={setTotalYears}  />
     </main>
